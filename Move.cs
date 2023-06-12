e2ea41cdbed09e51a2abe672fc9cf8968f094e53
Farahihab <85008571+Farahihab@users.noreply.github.com>
2023-06-12 15:46:15 +0300

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Othello
+{
+    public class Move
+    {
+        public int x = -1;
+        public int y = -1;
+    }
+}
